86cebfa311654acb03d185096336d603e74903da
Matt Fielding <matt@hippodigital.co.uk>
2023-06-08 11:12:58 +0100

    final routing logic for healthcare section

--- app/routes.js
+++ app/routes.js
@@ -20,6 +20,10 @@ router.use((req, res, next) => {
 router.post('/v3/personal-country-post', function (req, res) {
   let country = req.session.data['country']
 
+  if (! req.session.data['country'] ) {
+    req.session.data['country'] = 'England'
+  }
+
   if (country === "Scotland"){
     // Send user to interuption page
     res.redirect('/v3/personal-country-scotland')
@@ -41,22 +45,32 @@ router.post('/v3/personal-features-post', function (req, res) {
 
 router.post('/v3/healthcare-do-you-know-nhs-no-post', function (req, res) {
   let nhs = req.session.data['knows-nhs-no']
+  let change = req.session.data['change']
 
-  if (nhs === "Yes"){
-    // Send user to nhs number input page
+  if (nhs === "No" && change === 'true'){
+    // Send user to summary page
+    res.redirect('/v3/check-details-1')
+  } else if (nhs === "Yes"){
+    // Send user to number input page
     res.redirect('/v3/healthcare-nhs-no')
   } else {
+    // go to next step in the journey
     res.redirect('/v3/healthcare-gp-surgery')
   }
 })
 
 router.post('/v3/healthcare-do-you-know-chi-no-post', function (req, res) {
   let chi = req.session.data['knows-chi-no']
+  let change = req.session.data['change']
 
-  if (chi === "Yes"){
-    // Send user to nhs number input page
+  if (chi === "No" && change === 'true'){
+    // Send user to summary page
+    res.redirect('/v3/check-details-1')
+  } else if (chi === "Yes"){
+    // Send user to number input page
     res.redirect('/v3/healthcare-chi-no')
   } else {
+    // go to next step in the journey
     res.redirect('/v3/healthcare-gp-surgery')
   }
 })
@@ -64,7 +78,7 @@ router.post('/v3/healthcare-do-you-know-chi-no-post', function (req, res) {
 
 router.post('/v3/healthcare-lpa-post', function (req, res) {
   let lpa = req.session.data['has-lpa']
-
+  // basic routing
   if (lpa === "Yes"){
     // Send user to nhs number input page
     res.redirect('/v3/healthcare-attorney-add')
@@ -73,18 +87,79 @@ router.post('/v3/healthcare-lpa-post', function (req, res) {
   }
 })
 
+// generate back link
+router.get('/v3/healthcare-attorney-add', function (req, res) {
+  let attorneys = req.session.data['attorneys']
+  // set the variables as blank initially
+  let attorneyName = ''
+  let attorneyEmail = ''
+  let attorneyPhone = ''
+
+  // grab the previous page so we can create a contextual back link
+  res.locals.prevURL = req.get('Referrer')
+  prevURL = res.locals.prevURL
+
+  // grab query string from url if it exists
+  let item = req.query.item
+
+  // if making an edit pull back the previous details and populated the variables
+  if (req.query.item) {
+    attorneyName = attorneys[item][0]
+    attorneyEmail = attorneys[item][1]
+    attorneyPhone = attorneys[item][2]
+  }
+
+  req.session.data['amend-attorney'] = ''
+
+  // send back the variables to the page
+  return res.render('v3/healthcare-attorney-add', {
+    'prevURL': prevURL,
+    'item': item,
+    'attorneyName': attorneyName,
+    'attorneyEmail': attorneyEmail,
+    'attorneyPhone': attorneyPhone,
+  })
+})
+
+// add or amend new attorney details
 router.post('/v3/healthcare-attorney-add-post', function (req, res) {
   // load in the array if it exists already
   let attorneys = req.session.data['attorneys'] || []
 
-  // push the value from the input field into the array
-  attorneys.push(req.session.data['attorney-name'])
+  // determine if add or edit
+  let item = req.session.data['amend-attorney']
+
+  let attorneyName = req.session.data['attorney-name']
+  let attorneyEmail = req.session.data['attorney-email']
+  let attorneyPhone = req.session.data['attorney-telephone-number']
+  // grab the inputted details and store them in our new array
+  let newDetails = [attorneyName,attorneyEmail,attorneyPhone]
+
+  if (item !== '') { // is an edit
+    // overrite the values in the array
+    attorneys[item] = newDetails
+    console.log('item found')
+  } else { // is a new addition
+    // push the values from the input fields as an array into the array
+    attorneys.push(newDetails)
+  }
+
 
   // push the array back into the session data
   req.session.data['attorneys'] = attorneys
 
   // move to the summary page
   res.redirect('/v3/healthcare-attorney-list')
+})
+
+// generate back link
+router.get('/v3/healthcare-attorney-list', function (req, res) {
+  res.locals.prevURL = req.get('Referrer')
+  prevURL = res.locals.prevURL
+
+  return res.render('v3/healthcare-attorney-list', {
+    'prevURL': prevURL
+  })
 })
 
 router.post('/v3/healthcare-attorney-list-post', function (req, res) {
@@ -98,6 +173,35 @@ router.post('/v3/healthcare-attorney-list-post', function (req, res) {
     res.redirect('/v3/check-details-1')
   }
 })
+
+router.post('/v3/healthcare-attorney-list-remove', function (req, res) {
+  // pull in the array values
+  var attorneys = req.session.data['attorneys']
+
+  // pick up the value of the specific remove link
+  let remove = req.session.data.remove
+
+  // remove 1 item from the array at the place indicated by the remove link
+  attorneys.splice(remove, 1);
+
+  // reload the page
+  res.redirect('/v3/healthcare-attorney-list')
+
+})
+
+// generate back link
+router.get('/v3/check-details-1', function (req, res) {
+  res.locals.prevURL = req.get('Referrer')
+  prevURL = res.locals.prevURL
+
+  return res.render('v3/check-details-1', {
+    'prevURL': prevURL
+  })
+})
+
+
+
+
 
 
 // V2 ROUTES
